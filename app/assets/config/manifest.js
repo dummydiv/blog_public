cf1f9879a566693368d0f04d123d46a3c51499d1
Dummy Div <dummydiv127@gmail.com>
2024-10-17 14:26:35 +0530

    Changed Multiple Files Using Octokit Client

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,9 +1,1 @@
-//= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
-Hello This is Updated File
-This is the file.
-This is the new commit
-
-This is the latest commit which will be pushed to git.
+MANIFEST.JS NEW CHANGE
